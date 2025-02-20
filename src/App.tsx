9a24ffcc19d02ee92b14af58fc11e534ab5b3b70
Takane Ichinose <ichinose.takane@gmail.com>
2025-02-21 03:32:56 +0900

    Added favicons and banners for SEO

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      TEST
+    </>
+  )
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+import { myProfile } from './vite_plugins/vite-plugin-my-profile';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    myProfile(),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
+})
